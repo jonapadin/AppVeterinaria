b07aabf1d5d7c9e4795ba34d924ad932ad7921e5
marina <mari68253@gmail.com>
2025-09-22 21:21:33 -0300

    has been component footer

---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,24 @@
+
+function Footer(){
+    return(
+        <footer>
+            <div className="footer">
+                < img src ="../../assets/icons/patitaGatoPerroLogo 2.svg" alt="patitas"/>
+                
+            </div>
+            <div ><ul>
+                <li>
+                    inicio
+                </li>
+                <li>
+                    servicios
+                </li>
+             <li>
+                 contacto
+             </li>
+            </ul>
+                </div>
+        </footer>
+    )
+}
+export default Footer
