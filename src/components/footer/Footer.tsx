a6efca8a38822eec6db4cbf60ed6c273871efc15
marina68253 <mari68253@gmail.com>
2025-10-02 17:28:33 -0300

    se modifico el footerpc los sTyle

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -30,52 +30,63 @@ export default function Footer() {
         </ul>
       </footer>
 
-     {/* Footer para escritorio */}
-<footer className="hidden sm:flex fixed bottom-0 left-0 w-full h-45 bg-[#8F108D] text-white items-center md:px-[60px] lg:px-[150px]">
+<footer className="hidden sm:flex fixed bottom-0 left-0 w-full h-44 bg-[#8F108D] text-white items-center md:px-[60px] lg:px-[150px]">
 
 
-  <div className="flex items-center ">
-    <img src="../../assets/icons/logoVet.png" alt="Logo Veterinaria" className="w-60 h-30 mt-4" />
+  {/* Logo fijo */}
+  <div className="absolute left-4 md:left-8 lg:left-14">
+    <img 
+      src="../../assets/icons/logoVet.png" 
+      alt="Logo Veterinaria" 
+      className="w-40 h-22 md:w-60 md:h-30 lg:w-72 lg:h-36" 
+    />
   </div>
 
+ 
+  <div className="flex w-full justify-between items-center pl-[150px] md:pl-[200px] lg:pl-[260px]">
+<div className="flex flex-col items-center pt-4 gap-1
+                text-sm md:text-base lg:text-sm xl:text-lg
+                font-lato font-bold text-center mx-auto">
 
-  <div className="flex flex-col items-center mx-auto pt-4 gap-1 text-lg font-lato font-bold text-center">
-    <span>
-      Copyright © | 2025 | VETERINARIA<br/>
-      Todos los Derechos Reservados<br/>
-      Diseño y Desarrollo por<br/>
-      Celeste Ruspil · Jonathan Padin · Nicolas Dume · Marina Briceño
-    </span>
-  </div>
-
-
-  <div className="flex flex-col items-start gap-2 pt-4 font-lato font-bold">
-
-    <div className="flex items-center gap-3">
-      <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-        <BiLogoInstagramAlt className="w-8 h-8" />
-      </a>
-      <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-        <AiFillFacebook className="w-8 h-8" />
-      </a>
-      <span>Veterinaria</span>
+      <span>
+        Copyright © | 2025 | VETERINARIA<br/>
+        Todos los Derechos Reservados<br/>
+        Diseño y Desarrollo por<br/>
+        Celeste Ruspil · Jonathan Padin <br/>
+        · Nicolas Dume · Marina Briceño
+      </span>
     </div>
 
-    <div className="flex items-center gap-3">
-      <BiLogoGmail className="w-8 h-8" />
-      <span>veterinaria@gmail.com</span>
-    </div>
+   <div className="flex flex-col items-start gap-1 text-sm md:text-base lg:text-sm xl:text-lg
+font-lato font-bold ">
 
-    <div className="flex items-center gap-3">
-      <a href="https://wa.me/2284557768" target="_blank" rel="noopener noreferrer">
-        <BsWhatsapp className="w-8 h-8 text-green-500" />
-      </a>
-      <span>2284557768</span>
+      <div className="flex items-center gap-2">
+        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+          <BiLogoInstagramAlt className="w-8 h-8" />
+        </a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+          <AiFillFacebook className="w-8 h-8" />
+        </a>
+        <span>Veterinaria</span>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <BiLogoGmail className="w-8 h-8" />
+        <span>veterinaria@gmail.com</span>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <a href="https://wa.me/2284557768" target="_blank" rel="noopener noreferrer">
+          <BsWhatsapp className="w-8 h-8 text-green-500" />
+        </a>
+        <span>2284557768</span>
+      </div>
+
     </div>
 
   </div>
 
 </footer>
 </>
-  );
+);
 }
