2bdea137e8981d8cf5bb346dc06222e013e23c85
cele_ruspil <celesteruspil22@gmail.com>
2025-10-01 23:55:18 -0300

    cree carpeta pages y componente Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-
-import Footer from './components/footer/Footer'
-import NavBar from './components/navbar/NavBar'
+import Home from './pages/Home'
 function App() {
 
 
   return (
     <>
-    <NavBar/>
-    
-     <Footer/>
-    
+      <Home />
+
+
     </>
   )
 }
