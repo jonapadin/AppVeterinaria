678339bb6d7adcad0b5483eac3614019f894f2d1
cele_ruspil <celesteruspil22@gmail.com>
2025-10-13 20:55:47 -0300

    arregle navbar para que se vea responsivo

--- src/components/navbar/MenuMob.tsx
+++ src/components/navbar/MenuMob.tsx
@@ -6,11 +6,10 @@ type MenuMob = {
 
 export default function MenuMob({ onClose }: MenuMob) {
   return (
-    <div className="fixed inset-0 bg-black/50 z-40 flex justify-end">
+    <div className="fixed inset-0 bg-black/50 z-[999999]  flex justify-end">
       {/* Menú lateral derecho */}
-      <div className="bg-[#8F108D] text-white w-64 h-full p-6 shadow-lg">
-        {/* Botón de cerrar */}
-        <button onClick={onClose} className=" flex justify-end w-full">
+      <div className="bg-[#8F108D] text-white w-64 h-full p-6 shadow-lg z-[9999]">
+        <button onClick={onClose} className="flex justify-end w-full">
           <GoX className="w-6 h-6" />
         </button>
          <img src="../../assets/icons/logoVet.png" alt="Logo Veterinaria" className="w-40 h-20 mb-6 pl-3" />
