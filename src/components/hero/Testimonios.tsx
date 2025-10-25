99d70b0fc0b8fea3cb0d38a46eccd7f11c6e6fa8
marina68253 <mari68253@gmail.com>
2025-10-24 21:25:02 -0300

    se acomodo testimonios falta terminar el pantalla movil  algunas

--- src/components/hero/Testimonios.tsx
+++ src/components/hero/Testimonios.tsx
@@ -6,25 +6,25 @@ export interface Testimonio {
 
 function TestimonioLista({ testimonios }: { testimonios: Testimonio[] }) {
   return (
-    <section className="py-15">
-      <h3 className="text-center text-2xl md:text-3xl font-bold mb-8 uppercase">
+    <section className="py-10">
+      <h3 className="text-center text-2xl md:text-3xl font-bold mb-6 uppercase">
         Testimonios
       </h3>
 
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3
-       :grid-cols-3  gap-6 px-4 lg:max-w-7xl mx-auto">
+       xl:grid-cols-3 gap-3 sm:gap-3 md:gap-5 lg:gap-6 px-4 lg:max-w-7xl mx-auto">
         {testimonios.map((testimonio, index) => (
           <article key={index} className="relative rounded-lg ">
             {/* contenedor-superpuesto */}
-            <div className="relative w-full h-80 md:h-72 sm:h-50 lg:h-70 xl:h-80">
+            <div className="relative w-full md:h-72 h-70 sm:h-72 lg:h-70 xl:h-80">
               {/* Imagen de fondo */}
               <img
                 src={testimonio.imagenbackgroud}
                 alt="Fondo"
                 className="absolute inset-0
-                 lg:w-full sm:w-[30%] sm:h-[30%]
+                 sm:w-full sm:h-[100%]
                  md:h-[70%] md:w-full
-                 lg:h-[70%] 
+                 lg:h-[70%]  lg:w-full 
                  xl:h-[80%] object-cover z-0"
               />
 
@@ -32,9 +32,10 @@ function TestimonioLista({ testimonios }: { testimonios: Testimonio[] }) {
              <img
              src={testimonio.imagen}
              alt="Testimonio"
-             className="absolute top-0  z-[-1]
-             sm:w-1/3 sm:h-1/3
-              left-[50%] md:w-1/2  md:h-[60%]
+             className="  absolute top-0 left-1/2 z-[-1] 
+              w-[50%] h-[55%]            /* 👈 tamaño base: móvil (grande) */
+              sm:w-[50%] sm:h-[100%]   
+              md:w-1/2  md:h-[60%]
                lg:w-1/2 lg:h-[65%] 
                xl:w-1/2 xl:h-[75%]  
                "
@@ -44,10 +45,10 @@ function TestimonioLista({ testimonios }: { testimonios: Testimonio[] }) {
 
               {/* Texto */}
               <p className="absolute text-white font-bold text-center z-20  w-1/2
-               sm:top-1 sm-left.2 sm:text-xs
+               sm:top-1 sm-left-2 sm:text-[8px]
                md:top-1  md:left-2 md:text-xs 
                lg:top-6 lg:left-2 lg:text-sm
-               xl:top-4 xl:left-4 xl:text-base
+               xl:top-8 xl:left-2 xl:text-sm
 
   
               
